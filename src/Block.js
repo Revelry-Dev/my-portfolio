6793358c8d8e9c8979d1fc197f4106df4b97f615
Jamie <jmeleacc@gmail.com>
2022-11-30 20:05:34 -0600

    Fix build error

--- src/Block.js
+++ src/Block.js
@@ -4,6 +4,6 @@ export default function Block( { title, url }) {
 	
 	return (
 		
-		<div class="block"><a target="_blank" href={url} title={title}>{title}</a></div>
+		<div class="block"><a rel="noreferrer" target="_blank" href={url} title={title}>{title}</a></div>
 	)
 }
